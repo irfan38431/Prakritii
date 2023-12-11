481cbea77ebe6cefb16393d78677b42835ca35d1
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-12-11 22:55:00 +0530

    Add files via upload

---
+++ frontend/src/components/CaptureImage.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import CameraIcon from "./CameraIcon"; // Import your CameraIcon component
+
+type Props = {
+  handleCapture: (dataUri: string) => void;
+};
+
+const CaptureImage = ({ handleCapture }: Props) => {
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+  const handleOpenCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      setMediaStream(stream);
+      setIsCameraOpen(true);
+
+      // Display the camera stream in a video element
+      const videoElement = document.getElementById(
+        "camera-preview"
+      ) as HTMLVideoElement;
+      if (videoElement && stream) {
+        videoElement.srcObject = stream;
+        videoElement.play();
+      }
+    } catch (error) {
+      console.error("Error accessing the camera:", error);
+    }
+  };
+
+  const handleCaptureImage = () => {
+    const videoElement = document.getElementById(
+      "camera-preview"
+    ) as HTMLVideoElement;
+    if (videoElement) {
+      const canvas = document.createElement("canvas");
+      canvas.width = videoElement.videoWidth;
+      canvas.height = videoElement.videoHeight;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+        const dataUri = canvas.toDataURL("image/png");
+        handleCapture(dataUri);
+        setIsCameraOpen(false); // Close the camera view
+        setMediaStream(null); // Release the camera stream
+      }
+    }
+  };
+
+  return (
+    <div className="capture-image">
+      {!isCameraOpen && (
+        <CameraIcon
+          classText="text-red-500"
+          handleOpenCamera={handleOpenCamera}
+        />
+      )}
+
+      {isCameraOpen && (
+        <div className="camera-preview">
+          <video id="camera-preview" className="w-64 h-48" autoPlay muted />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CaptureImage;
