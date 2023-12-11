ccc8ec4885073f694028c79f06720ff791e458d2
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-12-11 16:29:34 +0530

    Add files via upload

---
+++ frontend/src/components/Contactus.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ContactusProps = {
+  onClick: () => void; // Define onClick function prop
+};
+
+const Contactus: React.FC<ContactusProps> = ({ onClick }) => {
+  return (
+    <div onClick={onClick}>
+      {/* Your SVG icon for Contactus */}
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+    </div>
+  );
+};
+
+export default Contactus;
