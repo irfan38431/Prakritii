110c48e385621f194e61d73a0f67887a42671399
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-12-11 16:31:31 +0530

    Update Menu.tsx

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,71 +1,52 @@
+// Import necessary packages and components
 import React, { useState } from "react";
-import Dropdown from "./Dropdown"; // Adjust the import path based on your folder structure
+import Livecon from "./livecon"; // Assuming this is your Livecon component
+import Contactus from "./Contactus"; // Assuming this is your Contactus component
+import MailIcon from "./mail"; // Import your Mail icon component
+import ChatIcon from "./chat"; // Import your Chat icon component
+import "./dropdown.css"; // Import your dropdown animation styles
 
 type MenuIconProps = {
   classText: string;
 };
 
 const MenuIcon: React.FC<MenuIconProps> = ({ classText }) => {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-  const [contactDropdownVisible, setContactDropdownVisible] = useState(false);
+  const [showContactOptions, setShowContactOptions] = useState(false);
 
   const handleLiveConsultation = () => {
-    window.location.href = "https://wa.me/918883685621";
+    window.open("https://wa.me/+918883685621", "_blank");
   };
+  
 
-  const handleContactUs = () => {
-    setContactDropdownVisible(!contactDropdownVisible);
+  const toggleDropdown = () => {
+    setShowContactOptions(!showContactOptions);
   };
 
   const handleContactViaWhatsApp = () => {
-    window.open("https://wa.me/918883685621","_blank");
+    window.open("https://wa.me/919445077270", "_blank");
   };
 
   const handleContactViaEmail = () => {
     window.open("mailto:ia383813@gmail.com", "_blank");
   };
 
-  const toggleDropdown = () => {
-    setDropdownVisible(!isDropdownVisible);
-  };
-
   return (
-    <div className="relative">
+    <div className="flex items-center">
       <button
-        className={`transition-all duration-300 ${classText}`}
-        onClick={toggleDropdown}
+        className={`transition-all duration-300 ${classText} mr-4`}
+        onClick={handleLiveConsultation}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
+        <Livecon classText={classText} />
       </button>
-      {isDropdownVisible && (
-        <Dropdown
-          items={[
-            { text: "->Live Consultation", onClick: handleLiveConsultation },
-            { text: "->ContactUs", onClick: handleContactUs },
-          ]}
-        />
-      )}
-      {contactDropdownVisible && (
-        <Dropdown
-          items={[
-            { text: "->Contact via WhatsApp", onClick: handleContactViaWhatsApp },
-            { text: "->Contact via Email", onClick: handleContactViaEmail },
-          ]}
-        />
-      )}
+      <Contactus onClick={toggleDropdown} />
+      <div className={showContactOptions ? "dropdown show" : "dropdown"}>
+        <button className="text-left flex items-center w-full py-2 px-4" onClick={handleContactViaWhatsApp}>
+          <ChatIcon classText="w-6 h-6 mr-2" /> WhatsApp
+        </button>
+        <button className="text-left flex items-center w-full py-2 px-4" onClick={handleContactViaEmail}>
+          <MailIcon classText="w-6 h-6 mr-2" /> Email
+        </button>
+      </div>
     </div>
   );
 };
