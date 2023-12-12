c566a7d197466a51d5199588ce571317a73e9a4c
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-12-12 14:53:25 +0530

    Update CaptureImage.tsx

--- frontend/src/components/CaptureImage.tsx
+++ frontend/src/components/CaptureImage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import CameraIcon from "./CameraIcon"; // Import your CameraIcon component
 
 type Props = {
