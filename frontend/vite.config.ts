047c66e10ccebf66e670996645380e9411e7624e
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-12-13 22:06:38 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,7 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {},
-    modules: false,
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "bootstrap/scss/bootstrap.scss";`,
-      },
-    },
-  },
-});
+})
