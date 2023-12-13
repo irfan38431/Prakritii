5c2a4cc6c4d2c17e9fe437ccc72833aa2bb0fecb
irfan38431 <77377738+irfan38431@users.noreply.github.com>
2023-12-13 21:45:08 +0530

    Update Controller.tsx

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect ,useRef} from "react";
-import 'bootstrap/dist/css/bootstrap.css'
 import Title from "./Title";
 import axios from "axios";
 import RecordMessage from "./RecordMessage";
